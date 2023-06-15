f3898dc7e62cc9dc8d189cb6f2628eb06a11f253
Cody Brown <cody@securityresearch.us>
2023-06-14 20:27:07 -0700

    Update and flatten contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    // goerli: {
+    //   gas: "auto",
+    //   gasPrice: "auto",
+    //   url: 'https://goerli.infura.io/v3/f92e5c4c343f48d7a98e62a2443a6956'
+    // },
+  },
   solidity: {
-    version: '0.8.9',
+    version: '0.8.18',
     settings: {
       optimizer: {
         enabled: true,
